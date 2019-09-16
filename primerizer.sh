7d32776d7a45970c35bea984d8a054714a37c418
LFAdams <xlincoln.adamsx@gmail.com>
2019-09-16 15:09:39 -0400

    Update primerizer.sh

--- primerizer.sh
+++ primerizer.sh
@@ -17,7 +17,8 @@ GENOME=/work/cemlab/reference_genomes/97103_v2.fa
 INPUT=/home/lfa81121/primerizer/testsnps.csv
 OUTPUT=/home/lfa81121/primerizer/potentialprimers.csv
 
-#Stores the current value for $IFS then changes the internal field seperator to a comma in order to read .csv columns.
+#Stores the current value for $IFS then changes the internal field seperator to
+#a comma in order to read .csv columns.
 OLDIFS=$IFS
 IFS=,
 
@@ -25,10 +26,16 @@ IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 #Creates the output file and writes the headers into the first row.
+#This should be changed to correspond to the columns of your .csv file, make
+#sure to include "POSLESS250,POSPLUS250,SNP1,SNP501,SNP27,SNP24,RSNP27,RSNP24"
+#at the end, as this sets the labels for the columns that will be created
 echo CHROM,POS,REF,ALT,AD_REFLOW,AD_ALTLOW,DPLOW,GQLOW,PLLOW,SNPINDEXLOW,AD_REFHIGH,AD_ALTHIGH,DPHIGH,GQHIGH,PLHIGH,SNPINDEXHIGH,REF_FRQ,DELTASNP,NSNPS,TRICUBEDELTASNP,G,GPRIME,PVALUE,NEGLOG10PVAL,QVALUE,MINDP,TRICUBEDP,CL95,CL99,POSLESS250,POSPLUS250,SNP1,SNP501,SNP27,SNP24,RSNP27,RSNP24 > $OUTPUT
 
-#Reads each line of the .csv and calculates target positions in geneome. Target positions are extracted from $GENOME and printed into $OUTPUT along with all columns from $INPUT.
-#Note that we must change $IFS back to its original value in order to output to the .csv, and then return it to "," again to restart the loop.
+#Reads each line of the .csv and calculates target positions in geneome. Target
+#positions are extracted from $GENOME and printed into $OUTPUT along with all
+#columns from $INPUT.
+#Note that we must change $IFS back to its original value in order to output to
+#the .csv, and then return it to "," again to restart the loop.
 while read CHROM POS REF ALT AD_REFLOW AD_ALTLOW DPLOW GQLOW PLLOW SNPINDEXLOW AD_REFHIGH AD_ALTHIGH DPHIGH GQHIGH PLHIGH SNPINDEXHIGH REF_FRQ DELTASNP NSNPS TRICUBEDELTASNP G GPRIME PVALUE NEGLOG10PVAL QVALUE MINDP TRICUBEDP CL95 CL99
   do
     IFS=$OLDIFS
