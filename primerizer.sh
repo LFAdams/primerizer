e7d9a9f0fcd00b78872088bf28505c4244493396
LFAdams <xlincoln.adamsx@gmail.com>
2019-09-16 16:17:17 -0400

    Update primerizer.sh

--- primerizer.sh
+++ primerizer.sh
@@ -15,7 +15,7 @@ module load SAMtools/1.6-foss-2016b
 #Set reference genome, input file path, and output file path
 GENOME=/work/cemlab/reference_genomes/97103_v2.fa
 INPUT=/home/lfa81121/primerizer/testsnps.csv
-OUTPUT=/home/lfa81121/primerizer/potentialprimers.csv
+OUTPUT=/home/lfa81121/primerizer/potentialprimers
 
 #Stores the current value for $IFS then changes the internal field seperator to
 #a comma in order to read .csv columns.
